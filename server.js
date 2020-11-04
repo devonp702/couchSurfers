977a4d32958f2f1c29cf51ed6dff40a1bd4d81a8
Jessica Anocibar <jessicayaws@yahoo.com>
2020-11-04 12:27:40 -0700

    made server file

---
+++ server.js
@@ -0,0 +1,33 @@
+//THIS FILE IS THE INITIAL STARTING POINT FOR NODE/EXPRESS SERVER//
+
+let express = require("express");
+let exphbs = require("express-handlebars");  
+const db = require("./models/index.js");
+let app = express();
+
+
+let PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+app.use(express.static("public"));
+
+
+let routes = require("./controllers/travelControllers.js")
+app.use(routes);
+
+
+
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
+
