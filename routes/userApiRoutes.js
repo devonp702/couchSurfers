4f733a1f9bac8796f93aa9b1fb25bff1d7c16fd9
Laura Cole <lauracole1900@comcast.net>
2020-11-05 14:29:15 -0700

    eliminated duplicate routes

--- routes/userApiRoutes.js
+++ routes/userApiRoutes.js
@@ -5,51 +5,33 @@
 
 var db = require("../models");
 
-module.exports = function(app) {
-    // Find single user by id and include all of their favorites
-  app.get("/api/:users/favorites", function(req, res) {
+module.exports = function (app) {
+  // Find single user by id and include all of their favorites
+  app.get("/api/:users/favorites", function (req, res) {
     db.Users.findOne({
       where: {
         id: req.params.id
       },
       include: [db.Favorites]
-    }).then(function(dbUsers) {
+    }).then(function (dbUsers) {
       res.json(dbUsers);
     });
   });
 
   // Create user -- connect to user sign-up
-  app.post("/api/:users", function(req, res) {
-    db.Users.create(req.body).then(function(dbUsers) {
+  app.post("/api/:users", function (req, res) {
+    db.Users.create(req.body).then(function (dbUsers) {
       res.json(dbUsers);
     });
   });
 
-  // Create favorite
-  app.post("/api/:users/favorites", function(req, res) {
-    db.Favorites.create(req.body).then(function(dbFavorites) {
-      res.json(dbFavorites);
-    });
-  });
-
-  //Delete favorite
-  app.delete("/api/:users/favorites/:id", function(req, res) {
-    db.Favorites.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbFavorites) {
-      res.json(dbFavorites);
-    });
-  });
-
   // Delete user -- Favorites needs to associate in Favorites model
-  app.delete("/api/:users", function(req, res) {
+  app.delete("/api/:users", function (req, res) {
     db.Users.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function(dbUsers) {
+    }).then(function (dbUsers) {
       res.json(dbUsers);
     });
   });
