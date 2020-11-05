1d02386025526dad416f57bd942ef9065e258c70
Jessica Anocibar <jessicayaws@yahoo.com>
2020-11-05 11:21:51 -0700

    working on models

---
+++ models/user.js
@@ -0,0 +1,15 @@
+//id, name, url/ foreign key
+
+module.exports = function (sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+        name: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [1, 140]
+                //validation that checks our user is between 1 and 140 characters
+            }
+        },
+
+    })
+}
