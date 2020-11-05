20a101f3c545a66da44944b1649f72e28344ec2b
Blackjack2020 <69491789+devonp702@users.noreply.github.com>
2020-11-05 13:30:27 -0700

    updating handlebars

--- config/config.js
+++ config/config.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 module.exports = {
   'development': {
-    'username': process.env.DB_USER,
-    'password': process.env.DB_PASS,
-    'database': process.env.travel_db,
+    'username': process.env.DB_USERNAME,
+    'password': process.env.DB_PASSWORD,
+    'database': process.env.DB_DATABASE,
     'host': process.env.DB_HOST,
-    'port': process.env.localhost,
-
-    //change
+    'port': process.env.DB_PORT,
+    'dialect': 'mysql'
+  },
+  'production': {
+    'use_env_variable': 'JAWSDB_URL',
+    'dialect': 'mysql'
+  }
+};
