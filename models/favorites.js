3d57f4b5c0d2e19e97946e596b9d1290288d1c7c
Laura Cole <lauracole1900@comcast.net>
2020-11-06 12:59:16 -0700

    added a couple console logs & changed login.js emails to usernames

--- models/favorites.js
+++ models/favorites.js
@@ -1,23 +1,23 @@
 
 
 //Favorites table
-module.exports = function (sequelize, Sequelize) {
-    var Favorites = sequelize.define("Favorites", {
-        location: {type: Sequelize.STRING, allowNull: false},
-        url: {type: Sequelize.STRING},
-        foreignKey: {type: Sequelize.STRING}
-    });
+// module.exports = function (sequelize, Sequelize) {
+//     var Favorites = sequelize.define("Favorites", {
+//         location: {type: Sequelize.STRING, allowNull: false},
+//         url: {type: Sequelize.STRING},
+//         foreignKey: {type: Sequelize.STRING}
+//     });
 
-    Favorites.associate = function (models) {
-        Favorites.hasOne(models.User, {
-            onDelete: "cascade"
-        });
+//     Favorites.associate = function (models) {
+//         Favorites.hasOne(models.User, {
+//             onDelete: "cascade"
+//         });
 
-        Favorites.belongsTo(models.User, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
-    return Favorites;
-};
+//         Favorites.belongsTo(models.User, {
+//             foreignKey: {
+//                 allowNull: false
+//             }
+//         });
+//     };
+//     return Favorites;
+// };
--- routes/favoritesApiRoutes.js
+++ routes/favoritesApiRoutes.js
@@ -1,38 +1,38 @@
-// routes for primarily favorites table
+// routes for primarily entry table
 
 // db.Users refers to users table
-// db.Favorites refers to favorites table
+// db.Entry refers to entries table
 
 var db = require("../models");
 
 module.exports = function (app) {
-  // GET route for retrieving a single favorite
-  app.get("/api/:users/favorites/:id", function (req, res) {
-    db.Favorites.findOne({
+  // GET route for retrieving a single entry
+  app.get("/api/:users/entries/:id", function (req, res) {
+    db.Entry.findOne({
       where: {
         id: req.params.id
       },
       include: [db.Users]
-    }).then(function (dbFavorites) {
-      res.json(dbFavorites);
+    }).then(function (dbEntry) {
+      res.json(dbEntry);
     });
   });
 
-  // POST route for saving a new favorite
-  app.post("/api/:users/favorites", function (req, res) {
-    db.Favorites.create(req.body).then(function (dbFavorites) {
-      res.json(dbFavorites);
+  // POST route for saving a new entry
+  app.post("/api/:users/entries", function (req, res) {
+    db.Entry.create(req.body).then(function (dbEntry) {
+      res.json(dbEntry);
     });
   });
 
-  // DELETE route for deleting favorites
-  app.delete("/api/:users/favorites/:id", function (req, res) {
-    db.Favorites.destroy({
+  // DELETE route for deleting entries
+  app.delete("/api/:users/entries/:id", function (req, res) {
+    db.Entry.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function (dbFavorites) {
-      res.json(dbFavorites);
+    }).then(function (dbEntry) {
+      res.json(dbEntry);
     });
   });
 };
