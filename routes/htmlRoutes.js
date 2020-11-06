572500f75a97175f5a4c7e8f897a588ba0eabcfa
A Joseph <alecrewinkel@gmail.com>
2020-11-06 11:01:23 -0700

    html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,1 +1,24 @@
-//where renders happen
+
+// Dependencies
+var path = require("path");
+
+// Routes
+module.exports = function(app) {
+
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads view.html
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/favorites.html"));
+  });
+// travel route loads travel.html
+  app.get("/travel", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/travel.html"));
+  });
+
+  // favorites route loads favorites.html
+  app.get("/favorites", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/favorites.html"));
+  });
+
+};
