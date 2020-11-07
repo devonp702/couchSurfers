b15e02a71ffebb7b0901e766aa7a364b88d09693
Laura Cole <lauracole1900@comcast.net>
2020-11-06 23:09:17 -0700

    links between login & signup pages working, signup page not functioning yet

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   // Getting references to our form and input
   var signUpForm = $("form.signup");
-  var usernameInput = $("input#username-input");
-  var passwordInput = $("input#password-input");
+  var usernameInput = $("#username-input");
+  var passwordInput = $("#password-input");
 
   // When the signup button is clicked, we validate the username and password are not blank
   signUpForm.on("submit", function(event) {
@@ -11,25 +11,24 @@ $(document).ready(function() {
       username: usernameInput.val().trim(),
       password: passwordInput.val().trim()
     };
-
     if (!userData.username || !userData.password) {
       return;
-    }
+    } else {
     // If we have an username and password, run the signUpUser function
     signUpUser(userData.username, userData.password);
     usernameInput.val("");
     passwordInput.val("");
-  });
+  }});
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(username, password) {
-    $.post("/api/signup", {
-      username: username,
-      password: password
+  function signUpUser(usernameInput, passwordInput) {
+    $.post("/signup", {
+      username: usernameInput,
+      password: passwordInput
     })
       .then(function(data) {
-        window.location.replace("/members");
+        window.location.replace("../../members.html");
         // If there's an error, handle it by throwing up a bootstrap alert
       })
       .catch(handleLoginErr);
