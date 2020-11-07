512541e148e58b7ae60b6a9651b649545d3b086b
Laura Cole <lauracole1900@comcast.net>
2020-11-06 23:14:14 -0700

    changes on signup.js, forgot to save, not working yet

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -1,31 +1,31 @@
 $(document).ready(function() {
-  // Getting references to our form and input
   var signUpForm = $("form.signup");
   var usernameInput = $("#username-input");
   var passwordInput = $("#password-input");
 
-  // When the signup button is clicked, we validate the username and password are not blank
+  // Event listener and validation that inputs are not blank
   signUpForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
       username: usernameInput.val().trim(),
       password: passwordInput.val().trim()
     };
+    console.log(userData);
+    // If either input is blank, return
     if (!userData.username || !userData.password) {
       return;
-    } else {
-    // If we have an username and password, run the signUpUser function
+    }
+    // If both inputs have been entered, run the signUpUser function
     signUpUser(userData.username, userData.password);
     usernameInput.val("");
     passwordInput.val("");
-  }});
+  });
 
-  // Does a post to the signup route. If successful, we are redirected to the members page
-  // Otherwise we log any errors
-  function signUpUser(usernameInput, passwordInput) {
+  // POST to the signup route. If successful, redirect to the members page; otherwise, log any errors
+  function signUpUser(username, password) {
     $.post("/signup", {
-      username: usernameInput,
-      password: passwordInput
+      username: username,
+      password: password
     })
       .then(function(data) {
         window.location.replace("../../members.html");
