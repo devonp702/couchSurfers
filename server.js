ce820e711c1967c7573c05be10f6747a32c38596
Jessica Anocibar <jessicayaws@yahoo.com>
2020-11-10 12:55:45 -0700

    pulling new code

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.set("view engine", "handlebars");   //replace
 
 
 app.use(express.static("public"));
